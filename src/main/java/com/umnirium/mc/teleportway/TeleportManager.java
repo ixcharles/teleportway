9a1b35eb167b4de95ad0a51d404f8f58a67a0af2
ixcharles <its.charleswalker0@gmail.com>
2025-04-30 18:24:18 +0100

    Remove unused imports

--- src/main/java/com/umnirium/mc/teleportway/TeleportManager.java
+++ src/main/java/com/umnirium/mc/teleportway/TeleportManager.java
@@ -2,7 +2,6 @@ package com.umnirium.mc.teleportway;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.NamespacedKey;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
